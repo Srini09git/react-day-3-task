3479119f032d7d0b61105debf7807bce14fadb9f
Srini09git <srinimohand@gmail.com>
2022-12-25 13:21:53 +0530

    day 3

---
+++ src/Cart.js
@@ -0,0 +1,17 @@
+export function Cart({ cartList, removeCart }) {
+    return (
+        <ol className="list-group list-group-numbered">
+            {cartList.map((item) => {
+                return (
+                    <li className="list-group-item d-flex justify-content-between align-items-start" >
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">{item.title}</div>
+                            {item.price}
+                        </div>
+                        <button onClick={() => removeCart(item)} className="badge bg-primary rounded-pill">Remove</button>
+                    </li>
+                )
+            })}
+        </ol>
+    );
+}
